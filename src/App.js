09e3c184d901dfcae82bd9e309d497888ff0a856
noormohammad20 <mdsaim204@gmail.com>
2022-03-19 21:54:14 +0600

    componenst and props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import Device from './Compotents/Device/Device'
 
 function App() {
   return (
     <div className="App">
-
+      <Device name="uPhone" price="13000"></Device>
     </div>
   )
 }
